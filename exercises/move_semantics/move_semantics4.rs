aaae665ca8119bcfc59f9956572f12c54a0b9cb2
dzwduan <dzwduan@gmail.com>
2022-01-08 21:40:38 +0800

    finish move_semantics

--- exercises/move_semantics/move_semantics4.rs
+++ exercises/move_semantics/move_semantics4.rs
@@ -7,9 +7,9 @@
 // I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec();
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -20,11 +20,12 @@ fn main() {
 
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument
 fn fill_vec() -> Vec<i32> {
-    let mut vec = vec;
+    let mut vec0 = Vec::new();
+    let mut vec = &mut vec0;
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
+    vec.to_vec()
 }
