5b1b2edc29b32288e2492ad10d1ea414557a7805
dzwduan <dzwduan@gmail.com>
2022-01-09 11:09:27 +0800

    finish struct

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -6,6 +6,8 @@
 
 // I AM NOT DONE
 
+
+
 #[derive(Debug)]
 struct Package {
     sender_country: String,
@@ -14,9 +16,10 @@ struct Package {
 }
 
 impl Package {
+
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            // Something goes here...
+            panic!("this is a terrible mistake!");
         } else {
             return Package {
                 sender_country,
@@ -26,12 +29,21 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        if self.weight_in_grams <= 0 {
+            return false;
+        }
+
+        if self.sender_country == "Spain" && self.recipient_country == "Russia" {
+            return true;
+        }else {
+            return false;
+        }
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
         // Something goes here...
+        self.weight_in_grams * cents_per_gram
     }
 }
 
@@ -73,7 +85,7 @@ mod tests {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
 
-        let cents_per_gram = ???;
+        let cents_per_gram = 3;
 
         let package = Package::new(sender_country, recipient_country, 1500);
 
